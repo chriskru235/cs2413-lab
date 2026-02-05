7b8e70ae907b774f89fbed4b2f6fb154e2af3978
chriskru235 <chris@svtop.localdomain>
2026-02-05 09:56:55 -0600

    indentation

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -3,24 +3,22 @@
 int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, int* out_k) {
     
     // implement your code here
-    int i=0;
-    // (j = L; k = R)
-    for(;i<n;i++) {
-	int j=i+1, k=n-1;
-	while(k > j) {
-             int sum = nums[i] + nums[j] + nums[k];
-	     if(sum < 0) j++; // L is too low
-	     else if(sum > 0) k--; // R is too high 
-	     else { // sum is equal to 0
-                 *out_i = i;
-		 *out_j = j;
-		 *out_k = k;
-		 return 1;
-	     }
+    	int i=0;
+    	// (j = L; k = R)
+	for(;i<n;i++) {
+		int j=i+1, k=n-1;
+		while(k > j) {
+			int sum = nums[i] + nums[j] + nums[k];
+			if(sum < 0) j++; // L is too low
+			else if(sum > 0) k--; // R is too high
+			else { // sum is equal to 0
+				*out_i = i;
+				*out_j = j;
+				*out_k = k;
+				return 1;
+			}
+		} 
 	}
-    }
-
-
 
     return 0;
 }
